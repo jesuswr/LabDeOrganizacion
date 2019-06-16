1451942da0434072239535e28e75e2da9dfc0847
jesuswr <jesuswahrman@gmail.com>
2019-06-16 13:57:50 -0400

    break 0x20 anadidos

--- proyecto 2/proyecto 2 2.0/moveInstructiones.s
+++ proyecto 2/proyecto 2 2.0/moveInstructiones.s
@@ -23,14 +23,34 @@ moveInstructions:
 		addi 	$s1, $s1, 4
 		j 	whileToGetToEnd
 	exitGetToEnd:
+		
+		addi	$s3, $s0, 0
 	
+	whilePlaceBreakAtEnd:
+		lw	$s2, ($s3)				
+		beqz	$s2, placeBreak				# Si la instruccion es un nop, la cambiamos por break 0x20
+		j 	continue
+		
+		placeBreak:
+			addi	$s2, $zero, 0x0000080d
+			sw	$s2, ($s3)			# Guardamos el break 0x20 
+			
+			addi	$s3, $s3, 4
+			j whilePlaceBreakAtEnd
+		
+		
+	continue:
+		
 		addi	$s0, $s0, -4			# Esta seria la direccion de la ultima instruccion
 		addi	$s1, $s1, -4			# Esta seria la direccion que contiene que tanto se debe mover la instruccion
+	
 	
 	whileToMove:
 		beq	$s0, $a0, exitMovement
 		
 		lw	$s3, ($s0)			# cargamos la instruccion en $s3
+		li	$s4, 0x0000080d
+		sw	$s4, ($s0)			# guardamos la instruccion break 0x20
 		lw	$s4, ($s1)			# cargamos que tanto hay que moverla en $s4
 		add	$s4, $s4, $s0			# le sumamos a $s4 la ubicacion de la instruccion
 		
